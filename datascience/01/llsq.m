13bfe14fb81bf8969a084e2db3fb3c4ea7db7c75
matvey <hodovanyuk.mi@gmail.com>
2016-09-12 17:27:31 +0300

    Update llsq.m

--- datascience/01/llsq.m
+++ datascience/01/llsq.m
@@ -3,29 +3,32 @@ function [S] = llsq(X,Y,p)
 N = length(X);
 
 M_X = ones(p,p);
-M_Y = ones([1;p])';
 
+M_Y = ones([1 p]);
+% p:-1:1
 for i =1:p
   for j =1:p
     M_X(i,j) = sum(X.^(i+j-2));
-  endfor
+  end
   M_Y(i) = sum(Y.*(X.^(i-1))); 
-endfor
+end
 
 M_X(1,1) = N;
 M_X = flipud(fliplr(M_X));
 M_Y = flipud(M_Y);
 % M1 * U = M2 => U = inv(M1) * M2
-U = M_X^(-1) * M_Y;
+% length(M_X)
+% length(M_Y)
+U = M_X^(-1) * M_Y';
 
-Y_NEW = ones([1;N]);
+Y_NEW = ones([1 N]);
 U = fliplr(U');
 for i=1:N
   for j = 2:length(U)
     Y_NEW(i) = U(j)*X(i)^(j-1);
-  endfor
+  end
   Y_NEW(i) = Y_NEW(i) + U(1); 
-endfor
+end
 
 S = (sum(Y-Y_NEW))^2
 plot(X,Y_NEW,'color',rand(1,3));
