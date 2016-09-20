250b771ca3f605fdf0f1d6ed6ba759d03a62ee60
matvey <hodovanyuk.mi@gmail.com>
2016-09-20 14:20:37 +0300

    Update main.m

--- os/01/main.m
+++ os/01/main.m
@@ -1,14 +1,15 @@
 clc
-f = fopen( 'data.txt' );
-i = 0;
+X = 2;
+Y = 2;
+f = fopen( 'data.txt.old.1_1' );
+k = 0;
 A = [];
 Clubs = [];
 while( ~feof( f ) )
     str = fgets( f );
     subs = regexp( str, ',', 'split' );
-%     A = [A subs];
-    i = i + 1;
-    A{ i } = subs;
+    k = k + 1;
+    A{ k } = subs(1:length(subs)-1);
     temp =  subs(2:length(subs)-1);
     for i = 1: length(temp)
         if length(Clubs) == 0
@@ -24,7 +25,23 @@ while( ~feof( f ) )
 end
 
 M = zeros([length(A),length(Clubs)]);
-
+UnianMatrix = zeros([1,length(Clubs)]);
 for i=1:length(A)
-    for j=1:length(Clubs)
-        if A{i,j}
+    for j=1:length(A{i})
+        if indexOf(Clubs,A{1,i}{1,j}) > 0
+            M(i, indexOf(Clubs,A{1,i}{1,j})) = 1;
+            UnianMatrix(indexOf(Clubs,A{1,i}{1,j})) = UnianMatrix(indexOf(Clubs,A{1,i}{1,j})) + 1;
+    
+        end
+    end
+%     for j=1:length(Clubs)
+%         disp(A{i,j})
+% %         if A{i,j}
+% %             disp(' i ->',i);
+% %         end
+%     end
+end
+
+% for i=1:length(M)
+%     for j=1:
+% end
