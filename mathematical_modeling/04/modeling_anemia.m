1ef0b3c1b0ab7929221dbd6d73c5d6db028750fc
matvey <hodovanyuk.mi@gmail.com>
2016-10-27 20:54:54 +0300

    added LR04

--- mathematical_modeling/04/modeling_anemia.m
+++ mathematical_modeling/04/modeling_anemia.m
@@ -1,5 +1,5 @@
 function modeling_anemia
-a = 0.001:1e-2:1;
+a = 0:1e-3:1;
 Na = length(a);
 N = 20;
 Nc = 220;
@@ -11,7 +11,7 @@ s = 16;
 t = 1:Nc;
 for i = 1:Na
     c = zeros(Nc,1);
-    c(1) = 1;
+    c(1) =1e-4;
     
     for n = 2:Nc
         c(n) = 4*a(i)*c(n-1)*(1-c(n-1));
@@ -22,7 +22,8 @@ for i = 1:Na
 end
 figure
 hold on;
-plot(a,cn,'k');
+plot(a,cn,'.k');
 xlabel('a');
 ylabel('cn');
+% print -djpg modeling_anemia0.jpg
 end
