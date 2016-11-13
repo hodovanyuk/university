4622e9a44ba2f4aa6c1f489d540935e7607fe68e
matvey <hodovanyuk.mi@gmail.com>
2016-11-13 20:20:18 +0200

    not done

--- ds/06/llsq.m
+++ ds/06/llsq.m
@@ -6,17 +6,28 @@ Y = [1.6 1.4 1.4 1.6 1.7 2 1.7 2.1];
 %m = (nX*sum(X.*Y)-sum(X)*sum(Y))/(nX*sum(X.^2)-(sum(X))^2)
 %c = (sum(Y)-m*sum(X))/(nX)
 
-%w = [0.5 0.5]
-%
-%t = sum(w.*[X 1]',2)
-%
-%gama = t .- Y'
-%
-%eta = 0.3
-%
-%dw = eta*gama.*X
-%
-%w = w + dw
+%init
+w = [0.5 0.5];
+eta = 0.3;
+% for step=1:10
+    for i= 1:nX
+        t = w(1)*X(1)+w(2);
+
+        gama = t - Y(1);
+
+        dw = eta*gama*[X(1) 1];
+
+        w = w + dw;
+    end
+% end
+x = 0:(X(nX)-X(1))/nX:X(nX);
+y = x*w(1)+w(2);
+grid on
+hold on
+plot(X,Y,'.-');
+plot(x,y,'.');
 
 
-bimod(X,Y)
+
+
+% bimod(X,Y)
