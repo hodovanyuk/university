615809dc4327d381d39495d3fb5b28f47d930960
matvey <hodovanyuk.mi@gmail.com>
2016-11-16 00:16:26 +0200

    almost done, it looks close but still not perfect

--- mm/05/wa_tor.m
+++ mm/05/wa_tor.m
@@ -1,87 +1,152 @@
 clc;clear all;close all;
+% old = zeros(N,N);
+% new = zeros(N,N);
+% 
+% old(N/2,N/2) = 1;
+% imh = image(cat(3,old,old,old));
+% while true
+%     for i = 1 : N
+%         for j = 1 : N
+%             if old(i,j)==1
+%                 x_n = i;
+%                 y_n = j+1;
+%                 if y_n > 64
+%                     y_n = 1;
+%                 end
+%                 
+%                 new(x_n,y_n) = 1;
+%             end
+% 
+%         end
+%     end
+%     old = new;
+%     new = zeros(N,N);
+%     set(imh,'cdata',cat(3,old,old,old))
+%     drawnow
+%     pause(.1);
+% end
+% init
 N = 64;
-
 old = zeros(N,N);
-new = zeros(N,N);
-
-old(N/2,N/2) = 1;
-imh = image(cat(3,old,old,old));
+new = old;
+f_step = @(t)[cos(deg2rad(t)),sin(deg2rad(t));-sin(deg2rad(t)),cos(deg2rad(t))];
+fbreed = 3;
+nstarve = 6;
+sbreed = 2;
+energy_point = 1;
+% add sharks and fishes with rand age
+for i=1:N
+    for j=1:N
+        r = rand;
+        if r>0.8
+            old(i,j) = 0;
+        elseif r<=0.8 && r >=0.3
+            old(i,j) = -1;
+        else
+            old(i,j) = 1;
+        end
+    end
+end
+old(1,1) = -1;
+old(1,2) = 1;
+imh = image(cat(3,(old==0),(old<0),(old>0)));
+pause(.5);
 while true
-    for i = 1 : N
-        for j = 1 : N
-            if old(i,j)==1
-                x_n = i;
-                y_n = j+1;
-                if y_n > 64
-                    y_n = 1;
+% for step =1:2
+    for i=1:N
+        for j=1:N
+        iup = [ (i-1<1)*N+(i>=1)*(i-1) j ];
+        iright = [ i (j+1>N)+(j+1<=N)*(j+1)];
+        idown = [ (i+1>N)+(i+1<=N)*(i+1) j ];
+        ileft = [ i (j-1<1)*N+(j>=1)*(j-1) ];
+        new_idx = [ iup; iright; idown; ileft ];
+    % fishes move
+            if old(i,j)>0
+    % on new layer move fish to new position
+                up = (old(iup(1),iup(2))==0);
+                right = 2*(old(iright(1),iright(2))==0);
+                down = 3*(old(idown(1),idown(2))==0);
+                left = 4*(old(ileft(1),ileft(2))==0);
+                pos = [up,right,down,left];
+                pos = find(pos~=0);
+                if ~isempty(pos)
+                    %there is  free space around
+                    rand_idx = round((length(pos)-1)*rand + 1);
+                    new(new_idx(pos(rand_idx), 1),new_idx(pos(rand_idx), 2)) = old(i,j) + 1;  
+                    % if old(i,j)/fbreed == 1 ->  put on old fish place one child 
+                    if mod( new(new_idx(pos(rand_idx), 1),new_idx(pos(rand_idx), 2)),fbreed) > 1
+                        new(i,j) = 1;
+                        new(new_idx(pos(rand_idx), 1),new_idx(pos(rand_idx), 2)) = mod( new(new_idx(pos(rand_idx), 1),new_idx(pos(rand_idx), 2)),fbreed);
+                    end
+                else
+                    %there is no free scpace around
+                    new(i,j) = old(i,j) + 1;
+                    if mod( new(i,j),fbreed) > 1
+                        new(i,j) = 1;
+                    end
+                end
+
+            end
+        
+            % sharks move
+            if old(i,j) < 0
+                % if fish is near the shark, shark moves on it place
+                % find fishes around shark
+                up = ( old(iup(1), iup(2)) > 0 );
+                right = 2*( old(iright(1), iright(2)) > 0 );
+                down = 3*( old(idown(1), idown(2)) > 0 );
+                left = 4*( old(ileft(1), ileft(2)) > 0 );
+                % make array of around state
+                pos = [up,right,down,left];
+                % let stay only places where fish is
+                pos = find(pos~=0);
+                % check if any fish around
+                if ~isempty(pos)
+                    % we have at least one fish around
+                    % make random choise of future place
+                    % shark defently not die from starve
+                    rand_idx = round((length(pos)-1)*rand + 1);
+                    new(new_idx(pos(rand_idx), 1),new_idx(pos(rand_idx), 2)) = old(i,j) - energy_point;
+                    % check if shark could produce breed
+                    if abs(new(new_idx(pos(rand_idx), 1),new_idx(pos(rand_idx), 2))) > sbreed
+                        if mod(abs(new(new_idx(pos(rand_idx), 1),new_idx(pos(rand_idx), 2))),sbreed) > 0
+                            new(i,j) = -1; 
+                            new(new_idx(pos(rand_idx), 1),new_idx(pos(rand_idx), 2)) = mod(abs(new(new_idx(pos(rand_idx), 1),new_idx(pos(rand_idx), 2))),sbreed);
+                        end
+                    end
+                else
+                    %check if shar still have energy point
+                    old(i,j) = old(i,j) + 1;
+                    if old(i,j) < 0
+                        % there is no fish around, let find free place for
+                        % shark
+                        up = (old(iup(1),iup(2))~=0);
+                        right =  2*(old(iright(1),iright(2))~=0);
+                        down = 3*(old(idown(1),idown(2))~=0);
+                        left = 4*(old(ileft(1),ileft(2))~=0);
+                        pos = [up,right,down,left];
+                        pos = find(pos~=0); 
+                        % check if there are free places around shark
+                        if ~isempty(pos)
+                            % there are free places around shark
+                            rand_idx = round((length(pos)-1)*rand + 1);
+                             new(new_idx(pos(rand_idx), 1),new_idx(pos(rand_idx), 2)) = old(i,j) - 1;
+                        else
+                            % there is no free places around shark, it will
+                            % stay on the same place
+                            new(i,j) = old(i,j) - 1;
+                        end
+                    end
+                    
                 end
                 
-                new(x_n,y_n) = 1;
             end
 
         end
     end
     old = new;
     new = zeros(N,N);
-    set(imh,'cdata',cat(3,old,old,old))
+    set(imh,'cdata',cat(3,(old==0),(old<0),(old>0)))
     drawnow
-    pause(.1);
-end
-% init
-old = zeros(N,N);
-f_step = @(t)[cos(deg2rad(t)),sin(deg2rad(t));-sin(deg2rad(t)),cos(deg2rad(t))];
-% add sharks and fishes with rand age
-for i=1:N
-    for j=1:N
-        if rand>0.3
-            old(i,j) = 1;
-        else
-            old(i,j) = -1;
-        end
-    end
-end
-
-for i=1:N
-    for j=1:N
-% fishes move
-        if old(i,j)>0
-% on new layer move fish to new position
-            up = 1 - (old(i-1,j)~=0);
-            right = 2 - 2*(old(i,j+1)~=0);
-            down = 3 - 3*(old(i+1,j)~=0);
-            left = 4 - 4*(old(i,j-1)~=0);
-            pos = [up,right,down,left];
-            pos = find(pos~=0);
-            r_idx = round((length(pos)-1)*rand + 1);
-            step =  round([0 1]*f_step(pos(r_idx)*90));
-            %case when new place is already filled
-            if new(step(1),step(2)) ~= 0
-                if rand>0.5
-                     new(step(1),step(2)) = old(i,j) + sqrt(old(i,j)^2)/old(i,j);
-                end
-            else 
-                new(step(1),step(2)) = old(i,j) + sqrt(old(i,j)^2)/old(i,j);
-            end            
-% if old(i,j)/fbreed == 1 ->  put on old fish place one child 
-            if old(i,j)/fbreed == 1
-                new(i,j) = 1;
-            end
-        end
-
-% sharks move
-        if old(i,j) < 0
-% if fish near shark, shark moves on it place
-           %
-           %
-           %
-           
-           % add torus 
-           
-           %
-           %
-           %
-% if shark haven't meet fish for nstave steps it die
-        end
-
-    end
+    pause(.5);
 end
