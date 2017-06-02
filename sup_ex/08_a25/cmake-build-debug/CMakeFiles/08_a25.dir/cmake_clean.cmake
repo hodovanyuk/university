003c10bebe4372b66aacd23604c2a23bd255edfd
matvii hodovaniuk <hodovanyuk.mi@gmail.com>
2017-06-02 17:35:30 +0300

    add 08_a25

---
+++ sup_ex/08_a25/cmake-build-debug/CMakeFiles/08_a25.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/08_a25.dir/main.c.o"
+  "08_a25.pdb"
+  "08_a25"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/08_a25.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
